1b00e80c6e2903c6904d69663a99a5794eced4d0
jakubmata <jakub@pop-os.localdomain>
2024-07-08 11:20:35 +0200

    Basic p tag displayed

--- script/viewer.go
+++ script/viewer.go
@@ -1,38 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 )
 
 var BORDER_COLOR color.Gray16 = color.Black
+var TEXT_COLOR color.Gray16 = color.Black
 
 func CreateViewer(root *TreeRoot) {
 	viewerApp := app.New()
 	window := viewerApp.NewWindow("Hello World")
-	window.Resize(fyne.NewSize(1000, 1000))
+	window.Resize(fyne.NewSize(800, 800))
 
 	mainContainer := root.Root.traverseParsingTree()
-	window.SetContent(mainContainer)
+	window.SetContent(container.NewScroll(mainContainer))
 
 	window.Show()
 	viewerApp.Run()
 }
 
 func (root *TreeVertex) traverseParsingTree() fyne.CanvasObject {
-	fmt.Println(root.Token.Name)
-	label := widget.NewLabel(root.Token.Name)
+
 	var childContainers []fyne.CanvasObject
 	for _, child := range root.Children {
 		childContainers = append(childContainers, child.traverseParsingTree())
 	}
 
-	subContainer := container.NewVBox(childContainers...)
-	baseContainer := container.NewVBox(label, subContainer)
+	rootContainer := containerFactory(root)
+	baseContainer := container.NewVBox(
+		rootContainer,
+		container.NewVBox(childContainers...),
+	)
 	return baseContainer
 }
--- script/viewerTags.go
+++ script/viewerTags.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
 type DefaultValues struct {
 	fontSize       uint16
 	textAlign      string
@@ -8,12 +17,31 @@ type DefaultValues struct {
 }
 
 var defaultValuesMap = map[string]DefaultValues{
-	"p": DefaultValues{
+	"p": {
 		fontSize: 16,
 		display:  "block",
 	},
 }
 
-func getBase(name string) DefaultValues {
-	return defaultValuesMap[name]
+func getDefaults(name string) (DefaultValues, bool) {
+	res, err := defaultValuesMap[name]
+	return res, err
+}
+
+func containerFactory(e *TreeVertex) fyne.CanvasObject {
+	defaultValues, included := getDefaults(e.Token.Name)
+	if !included {
+		return container.NewHBox(
+			widget.NewLabel(e.Token.Name),
+		)
+	}
+	switch e.Token.Name {
+	case "p":
+		label := canvas.NewText(e.Text.String(), TEXT_COLOR)
+		label.TextSize = float32(defaultValues.fontSize)
+		fmt.Println(e.Text.String())
+		return container.NewHBox(label)
+	}
+
+	return container.NewWithoutLayout()
 }
