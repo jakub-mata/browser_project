3b2b09506914fcdda1b3ea5fc35b1dc6ada4896e
jakubmata <jakub@pop-os.localdomain>
2024-08-12 21:17:33 +0200

    Web address arguments made positional

--- script/cli.go
+++ script/cli.go
@@ -10,10 +10,14 @@ var defaultSite string = "https://jakub-mata.github.io/"
 func startCLI() (bool, bool, string) {
 	logTokensBool := flag.Bool("log-tokens", false, "Logs tokenizer's output")
 	logParserBool := flag.Bool("log-parser", false, "Logs constructed parsing tree")
-	websiteToFetch := flag.String("address", defaultSite, "The web address to search")
+	//websiteToFetch := flag.String("address", defaultSite, "The web address to search")
 	flag.Parse()
+	websiteToFetch := defaultSite
+	if flag.Arg(0) != "" {
+		websiteToFetch = flag.Arg(0)
+	}
 	fmt.Println("tokens:", *logTokensBool)
 	fmt.Println("parser:", *logParserBool)
-	fmt.Println("website:", *websiteToFetch)
-	return *logTokensBool, *logParserBool, *websiteToFetch
+	fmt.Println("website:", websiteToFetch)
+	return *logTokensBool, *logParserBool, websiteToFetch
 }
